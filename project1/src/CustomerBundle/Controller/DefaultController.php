8f0a219a398a2740072eb5ad2b30fb81d9324501
loloromejko <alexandrepascal1@gmail.com>
2017-04-11 13:28:13 +0200

    changement de controller + base.html.twig + list.html.twig

--- project1/src/CustomerBundle/Controller/DefaultController.php
+++ project1/src/CustomerBundle/Controller/DefaultController.php
@@ -15,25 +15,30 @@ class DefaultController extends Controller
     public function listAction()
     {
         return new Response('Liste des clients');
+        $this->render('CustomerBundle:Default:list.html.twig');
     }
 
     public function createAction()
     {
         return new Response('Creation du client');
+        $this->render('CustomerBundle:Default:create.html.twig');
     }
 
     public function detailsAction($id)
     {
         return new Response('Details du client ' . $id);
+        $this->render('CustomerBundle:Default:details.html.twig');
     }
 
     public function updateAction($id)
     {
         return new Response('Mise a jour client ' . $id);
+        $this->render('CustomerBundle:Default:update.html.twig');
     }
 
     public function deleteAction($id)
     {
         return new Response('Supression du client ' . $id);
+        $this->render('CustomerBundle:Default:delete.html.twig');
     }
 }
