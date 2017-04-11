0cd4b4531316af5d41a8ff0ba348e144c2194ea1
loloromejko <alexandrepascal1@gmail.com>
2017-04-11 15:18:02 +0200

    Formulaire dans le controller

--- project1/src/CustomerBundle/Controller/PageController.php
+++ project1/src/CustomerBundle/Controller/PageController.php
@@ -3,6 +3,7 @@ namespace CustomerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
@@ -11,8 +12,34 @@ class PageController extends Controller
         return $this->render('CustomerBundle:Page:home.html.twig');
     }
 
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('CustomerBundle:Page:contact.html.twig');
+        $data = [
+            'email'=> null,
+            'subject'=> null,
+            'message'=> null,
+            'copy'=> null,
+            'send'=> null,
+        ];
+
+        $form = $this->createFormBuilder($data)
+            ->add('email')
+            ->add('subject')
+            ->add('message', 'textarea')
+            ->add('copy', 'checkbox')
+            ->add('send', 'submit')
+            ->getForm();
+
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid())
+                {
+                    $data=$form->getData();
+                    //return$this->redirectToRoute('');
+                }
+
+        return $this->render('CustomerBundle:Page:contact.html.twig', [
+            'form' => $form->createview(),
+        ]);
     }
 }
